4eea57e4857a8cfddc3e1e64c1b9488dd366e575
Wolves <cstingl@protonmail.com>
2022-10-25 09:05:28 -0400

    Move old config to .bak and begin new structure

---
+++ lua/config/mappings.lua
@@ -0,0 +1,37 @@
+---@diagnostic disable: missing-parameter
+
+local wk = require("which-key")
+local util = require("util")
+
+vim.o.timeoutlen = 300
+
+wk.setup({
+  show_help = false,
+  triggers = "auto",
+  plugins = { spelling = true },
+  key_labels = { ["<leader>"] = "SPC" },
+})
+
+-- Move to window using the <ctrl> movement keys
+vim.keymap.set("n", "<left>", "<C-w>h")
+vim.keymap.set("n", "<down>", "<C-w>j")
+vim.keymap.set("n", "<up>", "<C-w>k")
+vim.keymap.set("n", "<right>", "<C-w>l")
+
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<S-Up>", "<cmd>resize +2<CR>")
+vim.keymap.set("n", "<S-Down>", "<cmd>resize -2<CR>")
+vim.keymap.set("n", "<S-Left>", "<cmd>vertical resize -2<CR>")
+vim.keymap.set("n", "<S-Right>", "<cmd>vertical resize +2<CR>")
+
+-- Move Lines
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+
+-- Switch buffers with tab
+vim.keymap.set("n", "<C-Left>", "<cmd>bprevious<cr>")
+vim.keymap.set("n", "<C-Right>", "<cmd>bnext<cr>")
---
+++ lua/config/options.lua
@@ -0,0 +1,138 @@
+local indent =2
+
+local notify = {
+  old = vim.notify,
+  lazy = nil,
+}
+notify.lazy = function(...)
+  local args = { ... }
+  vim.defer_fn(function()
+    if vim.notify == notify.lazy then
+      notify.old(unpack(args))
+    else
+      vim.notify(unpack(args))
+    end
+  end, 300)
+end
+vim.notify = notify.lazy
+
+if vim.fn.has("nvim-0.8") == 1 then
+  --   vim.opt.spell = true -- Put new windows below current
+  vim.opt.cmdheight = 0
+
+  -- make all keymaps silent by default
+  local keymap_set = vim.keymap.set
+  vim.keymap.set = function(mode, lhs, rhs, opts)
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    return keymap_set(mode, lhs, rhs, opts)
+  end
+end
+
+if vim.fn.has("nvim-0.9.0") == 1 then
+  vim.opt.splitkeep = "screen"
+end
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- vim.g.node_host_prog = "/home/wlvs/.pnpm-global/5/node_modules/neovim/bin/cli.js"
+
+vim.opt.autowrite = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.conceallevel = 3
+vim.opt.confirm = true
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+
+vim.o.formatoptions = "jcroqlnt"
+
+vim.opt.guifont = "FiraCode Nerd Font:h12"
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.grepformat = "%f:%l:%c:%m"
+vim.opt.hidden = true
+vim.opt.ignorecase = true
+vim.opt.inccommand = "nosplit"
+vim.opt.joinspaces = false
+vim.opt.list = true
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.pumblend = 10
+vim.opt.pumheight = 10
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 8
+vim.opt.shiftround = true
+vim.opt.shiftwidth = indent
+vim.opt.showmode = false
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = indent
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 200
+vim.opt.wildmode = "longest:full,full"
+vim.opt.completeopt = "menu,menuone,noselect"
+vim.opt.wrap = false
+vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
+vim.opt.fillchars = {
+  --   horiz = "━",
+  --   horizup = "┻",
+  --   horizdown = "┳",
+  --   vert = "┃",
+  --   vertleft = "┫",
+  --   vertright = "┣",
+  --   verthoriz = "╋",im.o.fillchars = [[eob: ,
+  -- fold = " ",
+  foldopen = "",
+  -- foldsep = " ",
+  foldclose = "",
+}
+
+-- don't load the plugins below
+local builtins = {
+  "gzip",
+  "zip",
+  "zipPlugin",
+  "fzf",
+  "tar",
+  "tarPlugin",
+  "getscript",
+  "getscriptPlugin",
+  "vimball",
+  "vimballPlugin",
+  "2html_plugin",
+  "matchit",
+  "matchparen",
+  "logiPat",
+  "rrhelper",
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+}
+
+for _, plugin in ipairs(builtins) do
+  vim.g["loaded_" .. plugin] = 1
+end
+
+-- Use proper syntax highlighting in code blocks
+local fences = {
+  "lua",
+  -- "vim",
+  "json",
+  "typescript",
+  "javascript",
+  "js=javascript",
+  "ts=typescript",
+  "shell=sh",
+  "python",
+  "sh",
+  "console=sh",
+}
+vim.g.markdown_fenced_languages = fences
