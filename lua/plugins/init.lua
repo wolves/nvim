5f2f8ed726f84b2a1ad2870ca74e4229ad90a766
Wolves <cstingl@protonmail.com>
2023-01-03 16:58:04 -0500

    Add trouble/vim-matchup/devicons

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,11 +6,34 @@ return {
   "nvim-lua/plenary.nvim",
   "folke/which-key.nvim",
 
+  -- Theme: icons
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = { default = true },
+  },
+
   {
     "dstein64/vim-startuptime",
     cmd = "StartupTime",
     config = function()
       vim.g.startuptime_tries = 10
+    end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
+    config = {
+      auto_open = false,
+      use_diagnostic_signs = true, -- en
+    },
+  },
+
+  {
+    "andymass/vim-matchup",
+    event = "BufReadPost",
+    config = function()
+      vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
     end,
   },
 }
