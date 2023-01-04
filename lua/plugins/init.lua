f7803f37211aa28250a60e0fb3915020967acee9
Wolves <cstingl@protonmail.com>
2023-01-04 09:39:24 -0500

    Move a few util plugins into separate config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,6 @@
 return {
   "jose-elias-alvarez/typescript.nvim",
-  "nvim-lua/plenary.nvim",
   "folke/which-key.nvim",
-
-  {
-    "dstein64/vim-startuptime",
-    cmd = "StartupTime",
-    config = function()
-      vim.g.startuptime_tries = 10
-    end,
-  },
 
   {
     "folke/trouble.nvim",
---
+++ lua/plugins/utils.lua
@@ -0,0 +1,14 @@
+return {
+
+  -- measure startuptime
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
+
+  -- library for other plugins
+  "nvim-lua/plenary.nvim",
+}
