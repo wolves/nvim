e73404e7c09713bec779d1938919068c60fbb0db
Wolves <cstingl@protonmail.com>
2023-01-04 15:34:44 -0500

    Add nvim-spectre

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -34,6 +34,13 @@ return {
   },
 
   -- search/replace in multiple files
+  {
+    "windwp/nvim-spectre",
+    -- stylua: ignore
+    keys = {
+      { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+    },
+  },
 
   -- fuzzy finder
   {
