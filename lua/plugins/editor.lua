431560af3866ca94c8c5c92fef771950e82a1379
Wolves <cstingl@protonmail.com>
2023-01-04 15:37:23 -0500

    Add vim-illuminate

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -171,6 +171,20 @@ return {
     },
   },
 
+  -- references
+  {
+    "RRethy/vim-illuminate",
+    event = "BufReadPost",
+    config = function()
+      require("illuminate").configure({ delay = 200 })
+    end,
+    -- stylua: ignore
+    keys = {
+      { "]]", function() require("illuminate").goto_next_reference(false) end, desc = "Next Reference", },
+      { "[[", function() require("illuminate").goto_prev_reference(false) end, desc = "Prev Reference" },
+    },
+  },
+
   -- buffer remove
   {
     "echasnovski/mini.bufremove",
