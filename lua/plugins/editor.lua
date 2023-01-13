6786103189f6821984bcb84ae860e2a7529e5fa7
wlvs <wlvs@fwrk.tail1a89d.ts.net>
2023-01-12 19:46:20 -0500

    Move neogit config into editor

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -156,6 +156,32 @@ return {
     end,
   },
 
+  -- neogit
+  {
+    "TimUntersberger/neogit",
+    cmd = "Neogit",
+    opts = {
+      disable_commit_confirmation = true,
+      kind = "floating",
+      commit_popup = {
+        kind = "floating",
+      },
+      popup = {
+        kind = "floating",
+      },
+      signs = {
+        -- { CLOSED, OPENED }
+        section = { "", "" },
+        item = { "", "" },
+        hunk = { "", "" },
+      },
+      integrations = { diffview = true },
+    },
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<CR>", desc = "Neogit" },
+    },
+  },
+
   -- git signs
   {
     "lewis6991/gitsigns.nvim",
