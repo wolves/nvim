18f0e200a881a97325af058666da592f631fc609
Wolves <cstingl@protonmail.com>
2023-03-08 11:03:32 -0500

    lazy & kanagawa: update plugins & refactor kanagawa for updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,37 @@
-return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-
-    config = function()
-      local kanagawa = require("kanagawa")
-      local default_clrs = require("kanagawa.colors").setup()
-
-      kanagawa.setup({
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        variablebuiltinStyle = { italic = true },
-        specialReturn = true, -- special highlight for the return keyword
-        specialException = true, -- special highlight for exception handling keywords
-        transparent = false, -- do not set background color
-        dimInactive = true,
-        globalStatus = true,
-        colors = {},
-        overrides = {
-          CursorLine = { bg = default_clrs.winterBlue },
-        },
-      })
-      -- vim.cmd("colorscheme kanagawa")
-      kanagawa.load()
+local config = function()
+  require("kanagawa").setup({
+    compile = false,
+    undercurl = true, -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true },
+    statementStyle = { bold = true },
+    typeStyle = {},
+    -- variablebuiltinStyle = { italic = true },
+    -- specialReturn = true, -- special highlight for the return keyword
+    -- specialException = true, -- special highlight for exception handling keywords
+    transparent = false, -- do not set background color
+    dimInactive = true,
+    globalStatus = true,
+    colors = {},
+    overrides = function(colors)
+      return {
+        CursorLine = { bg = colors.palette.winterBlue },
+      }
     end,
-  },
+    theme = "wave",
+    background = {
+      dark = "wave",
+      light = "lotus",
+    },
+  })
+  vim.cmd.colorscheme("kanagawa")
+end
+
+return {
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000,
+  config = config,
+  build = "KanagawaCompile",
 }
