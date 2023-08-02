32c31a697c8e05621173a780e9d6c2cd0f98f97f
Wolves <cstingl@protonmail.com>
2023-08-01 20:06:04 -0400

    mini.splitjoin: Add misc plugins file w/ mini.splitjoin config

---
+++ lua/plugins/misc.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "echasnovski/mini.splitjoin",
+    enabled = false,
+    opts = {
+      mappings = {
+        toggle = "J",
+      },
+    },
+    keys = { { "J", desc = "Split/Join" } },
+  },
+}
