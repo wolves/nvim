307d5cbb07a16ec8b1d3b2aea1ce35150236f95d
Wolves <cstingl@protonmail.com>
2023-08-08 17:13:56 -0400

    Add octo and commented peek plugin for markdown

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,8 +1,12 @@
 return {
 
+  -- Github Issue/PR Reviews in-editor
+  { "pwntester/octo.nvim", opts = {}, cmd = "Octo" },
+
   -- neorg
   {
     "nvim-neorg/neorg",
+    enable = false,
     ft = "norg",
     opts = {
       load = {
@@ -15,6 +19,27 @@ return {
       },
     },
   },
+
+  -- markdown preview
+  -- {
+  --   "toppair/peek.nvim",
+  --   build = "deno task --quiet build:fast",
+  --   keys = {
+  --     {
+  --       "<leader>op",
+  --       function()
+  --         local peek = require("peek")
+  --         if peek.is_open() then
+  --           peek.close()
+  --         else
+  --           peek.open()
+  --         end
+  --       end,
+  --       desc = "Peek (Markdown Preview)",
+  --     },
+  --   },
+  --   opts = { theme = "light" },
+  -- },
 
   -- better diffs
   {
