afd8b254193b669dda61a0565bbb996c817ae3dc
Wolves <cstingl@protonmail.com>
2023-08-10 08:45:28 -0400

    nvim-cmp: update keybinds /add tabout plugin/ update plugins

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -30,37 +30,65 @@ return {
     version = false,
     event = "InsertEnter",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-emoji",
-      "saadparwaiz1/cmp_luasnip",
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/cmp-emoji" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "abecodes/tabout.nvim", opts = { ignore_beginning = false, completion = false } },
     },
     opts = function()
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
       local defaults = require("cmp.config.default")()
+
+      local function shift_tab(fallback)
+        if not cmp.visible() then
+          return fallback()
+        end
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        end
+      end
+
+      local function tab(fallback) -- make TAB behave like Android Studio
+        if not cmp.visible() then
+          return fallback()
+        end
+        if not cmp.get_selected_entry() then
+          return cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        end
+        if luasnip.expand_or_jumpable() then
+          return luasnip.expand_or_jump()
+        end
+        cmp.confirm()
+      end
+
       return {
         completion = {
           completeopt = "menu,menuone,noinsert",
         },
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end,
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i" }),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<S-CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<S-TAB>"] = cmp.mapping(shift_tab, { "i", "s" }),
+          ["<TAB>"] = cmp.mapping(tab, { "i", "s" }),
+          -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          -- ["<S-CR>"] = cmp.mapping.confirm({
+          --   behavior = cmp.ConfirmBehavior.Replace,
+          --   select = true,
+          -- }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
