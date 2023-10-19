59c442501d937cafbefa3823d510295bb26edf5a
Wolves <cstingl@protonmail.com>
2023-10-19 13:54:34 -0400

    templ: Add treesitter support for golang templ files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -113,6 +113,22 @@ return {
           },
         },
       })
+
+      -- Temple Treesitter Support
+      require("nvim-treesitter.parsers").get_parser_configs().templ = {
+        install_info = {
+          url = "https://github.com/vrischmann/tree-sitter-templ.git",
+          files = { "src/parser.c", "src/scanner.c" },
+          branch = "master",
+        },
+      }
+
+      vim.treesitter.language.register("templ", "templ")
     end,
+  },
+
+  {
+    "vrischmann/tree-sitter-templ",
+    ft = "templ",
   },
 }
