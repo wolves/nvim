5fd14eba7e80a30b955775d4daa9fb4a957491a1
Wolves <cstingl@protonmail.com>
2024-01-04 10:18:13 -0500

    treesitter: remove duplicate

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -78,11 +78,6 @@ return {
           use_virtual_text = true,
           lint_events = { "BufWrite", "CursorHold" },
         },
-        query_linter = {
-          enable = true,
-          use_virtual_text = true,
-          lint_events = { "BufWrite", "CursorHold" },
-        },
         playground = {
           enable = true,
           disable = {},
