e6f120eeb250c09b0bc7458f72b54fb35d18a23e
Wolves <cstingl@protonmail.com>
2024-05-17 08:17:58 -0400

    Fix completeopt value and remove dupe

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -65,9 +65,6 @@ return {
       end
 
       return {
-        completion = {
-          completeopt = "menu,menuone,noselect",
-        },
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
