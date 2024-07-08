daee4f35d0a8e4df68295cfa3ebd495f839ebb8f
Wolves <cstingl@protonmail.com>
2024-07-08 10:26:40 -0400

    better-escape: update config to new version

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -349,9 +349,15 @@ return {
       local esc = require("better_escape")
 
       esc.setup({
-        mapping = { "jk", "jj" }, -- a table with mappings to use
         timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-        clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+        mappings = {
+          i = {
+            j = {
+              k = "<Esc>",
+              j = "<Esc>",
+            },
+          },
+        },
         keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
       })
     end,
