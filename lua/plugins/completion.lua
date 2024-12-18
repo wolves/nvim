7bac7e50036a6ec611a4a5c107a56c4b5ce2b1f2
Wolves <cstingl@protonmail.com>
2024-12-18 09:13:53 -0500

    blink.cmp: Adjust default config
    - Add C-k to toggle docs
    - Add "kind" desc to right side

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -6,7 +6,18 @@ return {
     version = "v0.*",
 
     opts = {
-      keymap = { preset = "default" },
+      keymap = {
+        preset = "default",
+        ["<C-k>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
+      },
+
+      completion = {
+        menu = {
+          draw = {
+            columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "kind" } },
+          },
+        },
+      },
 
       appearance = {
         use_nvim_cmp_as_default = true,
