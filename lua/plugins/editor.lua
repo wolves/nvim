d0837fd5f3ccfd6c845fabfdeef8a45941dfb27f
Wolves <cstingl@protonmail.com>
2025-03-11 08:49:25 -0400

    Update mini.files open location / update plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -16,7 +16,8 @@ return {
       {
         "<leader>e",
         function()
-          require("mini.files").open(vim.uv.cwd(), true)
+          -- require("mini.files").open(vim.uv.cwd(), true)
+          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
         end,
         desc = "Open mini.files (cwd)",
       },
