07a5b88bc9330ce98499a14d84a3993db6dedd44
Wolves <cstingl@protonmail.com>
2025-03-31 08:55:20 -0400

    Update plugins & add terminal keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,16 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
+local delmap = vim.keymap.del
+
+vim.keymap.del("n", "<c-/>")
+delmap("t", "<C-/>", { desc = "Hide Terminal" })
 
 map({ "n", "v" }, "<leader>w", "<cmd>w!<CR>", { desc = "Save", remap = true })
 map({ "n", "v" }, "<leader>q", "<cmd>q!<CR>", { desc = "Quit", remap = true })
+
+map("n", "<c-\\>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+
+map("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
